b3e2ed4c7b703da4724e9f179fc3fa840b871d11
AndSem77 <andsemua@gmail.com>
2021-07-29 08:39:28 +0300

    product list complete

--- components/ProductFeed.js
+++ components/ProductFeed.js
@@ -1,7 +1,57 @@
-export default function ProductFeed() {
+import Product from './Product';
+
+export default function ProductFeed({ products }) {
   return (
-    <div>
-      <h1>Products</h1>
+    <div
+      className='grid grid-flow-row-dense
+     md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto'
+    >
+      {products
+        .slice(0, 4)
+        .map(({ id, title, description, price, category, image }) => (
+          <Product
+            key={id}
+            id={id}
+            title={title}
+            price={price}
+            description={description}
+            category={category}
+            image={image}
+          />
+        ))}
+      <img
+        className='md:col-span-full'
+        src='https://links.papareact.com/dyz'
+        alt=''
+      />
+      <div className='md:col-span-2'>
+        {products
+          .slice(4, 5)
+          .map(({ id, title, description, price, category, image }) => (
+            <Product
+              key={id}
+              id={id}
+              title={title}
+              price={price}
+              description={description}
+              category={category}
+              image={image}
+            />
+          ))}
+      </div>
+      {products
+        .slice(5, products.length)
+        .map(({ id, title, description, price, category, image }) => (
+          <Product
+            key={id}
+            id={id}
+            title={title}
+            price={price}
+            description={description}
+            category={category}
+            image={image}
+          />
+        ))}
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,6 @@ export default function Home({ products }) {
       <main className='max-w-screen-2xl mx-auto'>
         <Banner />
         <ProductFeed products={products} />
-        <p>{products}</p>
       </main>
     </div>
   );
@@ -25,6 +24,7 @@ export async function getServerSideProps(context) {
   const products = await fetch('https://fakestoreapi.com/products').then(
     (res) => res.json()
   );
+
   return {
     props: {
       products,
